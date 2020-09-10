c7be02685d3108fb16a12fb60fcdf4a8b1180e76
oneddol <wonseok88e@naver.com>
2020-09-10 17:39:19 +0900

    logo 연결

--- js/index.js
+++ js/index.js
@@ -3,10 +3,19 @@
     $('#content').load('main.html')
 
     $('.login_menu > a').on('click', function(e){
-        e.preventDefault
+        e.preventDefault()
         var url = $(this).attr('href')
         $('#container').remove()
         $('#content').load(url)
     })
+
+    $('h1_nav > h1 > a').on('click', function(e){
+        e.preventDefault()
+        var url = $(this).attr('href')
+        $('#container').remove()
+        $('#content').load(url)
+    })
+
+    $()
 
 })(jQuery)
