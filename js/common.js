bb7264f55ee8d5f82efbea3963b7cf36591ff802
oneddol <wonseok88e@naver.com>
2020-09-22 17:30:50 +0900

    0922-2

--- js/common.js
+++ js/common.js
@@ -36,31 +36,35 @@
         arrows: false
     })
 
-    // introduce 아티클 애니메이션 효과
-    $(window).scroll(function(){
-        var sct = $(this).scrollTop()
-        var intNear = $('.introduce').offset().top - $(this).height()/2
-        if(sct>=intNear){
-            for(var i=0; i<$('.info_box .column').length; i++){
-                $('.info_box .column').eq(i).css({
-                    animationDelay:i*0.5+'s'
-                }).addClass('animate__fadeInUp')
-            }
-        }   else    {
-            $('.info_box .column').removeClass('animate__fadeInUp')
+    
+// main - introduce 아티클 애니메이션 효과
+$(window).scroll(function(){
+    var sct = $(this).scrollTop()
+    var intNear = $('.introduce').offset().top - $(this).height()/2
+    if(sct>=intNear){
+        for(var i=0; i<$('.introduce .info_box .column').length; i++){
+            $('.introduce .info_box .column').eq(i).css({
+                animationDelay:i*0.5+'s'
+            }).addClass('animate__fadeInUp')
         }
+    }   else    {
+        $('.info_box .column').removeClass('animate__fadeInUp')
+    }
 
-        var curriNear = $('.curriInfo').offset().top - $(this).height()/2
-        if(sct>=curriNear){
-            $('.curriBox .column').eq(0).addClass('animate__fadeInLeft')
-            $('.curriBox .column').eq(1).addClass('animate__fadeIn')
-            $('.curriBox .column').eq(2).addClass('animate__fadeInRight')
-        }   else    {
-            $('.curriBox .column').eq(0).removeClass('animate__fadeInLeft')
-            $('.curriBox .column').eq(1).removeClass('animate__fadeIn')
-            $('.curriBox .column').eq(2).removeClass('animate__fadeInRight')
-        }
-    })
+    var curriNear = $('.curriInfo').offset().top - $(this).height()/2
+    if(sct>=curriNear){
+        $('.curriBox .column').eq(0).addClass('animate__fadeInLeft')
+        $('.curriBox .column').eq(1).addClass('animate__fadeInRight')
+        $('.curriBox .column').eq(2).addClass('animate__fadeInLeft')
+        $('.curriBox .column').eq(3).addClass('animate__fadeInRight')
+    }   else    {
+        $('.curriBox .column').eq(0).removeClass('animate__fadeInLeft')
+        $('.curriBox .column').eq(1).removeClass('animate__fadeInRight')
+        $('.curriBox .column').eq(2).removeClass('animate__fadeInLeft')
+        $('.curriBox .column').eq(3).removeClass('animate__fadeInRight')
+    }
+})
+
 
     // greeting.html
     $('.greeting_header .greeting .greet_nav > a').on('click', function(){
--- js/index.js
+++ js/index.js
@@ -60,32 +60,6 @@
         })
     }
 })
-// introduce 아티클 애니메이션 효과
-    $(window).scroll(function(){
-        var sct = $(this).scrollTop()
-        var intNear = $('.introduce').offset().top - $(this).height()/2
-        if(sct>=intNear){
-            for(var i=0; i<$('.info_box .column').length; i++){
-                $('.info_box .column').eq(i).css({
-                    animationDelay:i*0.5+'s'
-                }).addClass('animate__fadeInUp')
-            }
-        }   else    {
-            $('.info_box .column').removeClass('animate__fadeInUp')
-        }
-
-        var curriNear = $('.curriInfo').offset().top - $(this).height()/2
-        if(sct>=curriNear){
-            $('.curriBox .column').eq(0).addClass('animate__fadeInLeft')
-            $('.curriBox .column').eq(1).addClass('animate__fadeIn')
-            $('.curriBox .column').eq(2).addClass('animate__fadeInRight')
-        }   else    {
-            $('.curriBox .column').eq(0).removeClass('animate__fadeInLeft')
-            $('.curriBox .column').eq(1).removeClass('animate__fadeIn')
-            $('.curriBox .column').eq(2).removeClass('animate__fadeInRight')
-        }
-    })
-
 
 
 
