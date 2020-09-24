43edfbf89b39e54b779f2d1cc99ff8b2274523b5
oneddol <wonseok88e@naver.com>
2020-09-24 10:36:45 +0900

    0924_mainslide animation

--- js/common.js
+++ js/common.js
@@ -3,7 +3,7 @@
     // slick 이벤트
     $('.mainSlideInner').slick({
         autoplay: true, // 자동재생
-        autoplaySpeed: 3000, // 간격시간
+        autoplaySpeed: 3500, // 간격시간
         dots: true, // 동그라미버튼
         speed: 600, // 바뀌는시간(생략가능)
         slidesToShow: 1, // 보여질슬라이드수(생략가능)
@@ -13,7 +13,7 @@
         pauseOnFocus: false, // 동그라미번호버튼 클릭시 자동실행 멈춤여부
         cssEase: 'linear', // 속도함수(생략가능)
         draggable: true, // 마우스드래그시 슬라이드 교체가능여부(생략가능)
-        fade: false, // 슬라이드가 수평으로 이동하지 않고, 제자리에서 사라지고 나타남(생략가능)
+        fade: true, // 슬라이드가 수평으로 이동하지 않고, 제자리에서 사라지고 나타남(생략가능)
         arrows: true, // 좌우화살표 사용여부(생략가능)
         prevArrow: '<button class="prevArrow marrow"><i class="fas fa-angle-left"></i></button>',
         nextArrow: '<button class="nextArrow marrow"><i class="fas fa-angle-right"></i></button>',
